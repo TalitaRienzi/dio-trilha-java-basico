223cfa068f110dfc141f563d6c8383ad4d7aa61e
Talita Rienzi <talitarp134@gmail.com>
2023-06-05 22:05:00 -0300

    Tipos das variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+        
+        short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_DE_PI = 3.14;
+        //Quando escrevemos em caixa alta e com a tipo FINAL ela não pode ser alterada ao longo do codigo
+    }
+}
